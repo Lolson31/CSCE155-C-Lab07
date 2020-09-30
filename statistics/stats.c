cd42fef3e6491f1bef4db482b8a6138ef5f3a2d6
Lolson31 <69780105+Lolson31@users.noreply.github.com>
2020-09-29 21:17:25 -0500

    a

--- statistics/stats.c
+++ statistics/stats.c
@@ -4,37 +4,65 @@
 #include "stats.h"
 
 //TODO: fix the error in this function
-void readInArray(int *arr, int size) {
+void readInArray(int *arr, int size)
+{
   int i;
   printf("Enter your list of numbers: ");
-  for (i = 0; i < size; i++) {
-    scanf("%d", arr[i]);
+  for (i = 0; i < size; i++)
+  {
+    scanf("%d", *arr[i]);
   }
   return;
 }
 
-int * generateRandomArray(int size) {
+int * generateRandomArray(int size)
+{
   int * randomArr = malloc(sizeof(int) * size);
-  for(int i=0; i<size; i++) {
+  for(int i=0; i<size; i++)
+  {
     randomArr[i] = rand();
   }
   return randomArr;
 }
 
-void printArray(const int *arr, int size) {
+void printArray(const int *arr, int size)
+{
   printf("[");
-  for(int i=0; i<size-1; i++) {
+  for(int i=0; i<size-1; i++)
+  {
     printf("%d, ", arr[i]);
   }
   printf("%d]\n ", arr[size-1]);
 }
 
-double getMean(  ,  ) {
+double getMean(int *array, int length)
+{
+  int mean;
+  double sum;
+
+  readInArray(array, length)
+
+  for (i = 0; i < length; i++)
+  {
+    sum = array[i] + array[i +1];
+  }
+  mean = sum / length;
+
+  printf("%d\n", mean);
 }
 
-int getMin(  ,  ) {
+int getMin(  ,  )
+{
+
 }
 
-int getMax(  , ) {
+int getMax(  , )
+{
 
+}
+
+int int main(int argc, char const *argv[])
+{
+  /* code */
+  return 0;
 }
--- worksheet.md
+++ worksheet.md
@@ -2,7 +2,7 @@
 # CSCE 155E
 ## Lab 7.0 Worksheet
 
-Name(s) and Login(s):
+Name(s) and Login(s): Luke Olson Lolson31/Laolson
 
 
 
@@ -10,10 +10,11 @@ Name(s) and Login(s):
 
 a) How much memory did `memLeak` consume by the end of its
    execution?
-
+   120,001,024 bytes
 
 b) How much memory was "lost" according to Valgrind?
-
+  36,000,000
 
 c) How does that correlate to the parameters you ran
    (10 arrays of 1 million integers each)?
+   
